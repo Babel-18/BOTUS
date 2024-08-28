797926483f810653eb0410af9684d9be4f0b9992
Aevee Bee <32208148+Babel-18@users.noreply.github.com>
2024-08-27 20:36:23 -0700

    fix

--- DiscordBot/src/index.js
+++ DiscordBot/src/index.js
@@ -162,7 +162,6 @@ client.on('messageCreate',(message)=>{
     const genius_filter = (reaction, user) => reaction.emoji.name === '🤎';
     const collector = message.createReactionCollector({ genius_filter, max:1, time: 60_000, errors: ['time']  });
     message.reply(`${message.user.username}, you're a genius, and you worked so hard on this!`);
-    });
     collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 
 });
